b4a7e04bf7da450b321373efae83fc04f1ca010f
Dmytro Tarhan <diditar.com@gmail.com>
2019-02-12 19:53:22 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -1,57 +1,65 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".page-nav").addEventListener("click", function (e) {
-        let target = e.target;
-        if(!target.matches("a"))return;
-        [...document.querySelectorAll(".page-nav a")].forEach(function (link) {
-            link.style.borderColor = "#0070AF";
-        });
-        target.style.borderColor = "white";
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".page-nav").addEventListener("click", function(e) {
+    let target = e.target;
+    if (!target.matches("a")) return;
+    [...document.querySelectorAll(".page-nav a")].forEach(function(link) {
+      link.style.borderColor = "#0070AF";
     });
+    target.style.borderColor = "white";
+  });
 
-    function Slider() {
-        let curentSlider = 0;
-        let getImages = document.querySelectorAll(".slider__image-container");
+  function Slider() {
+    let curentSlider = 0;
+    let getImages = document.querySelectorAll(".slider__image-container");
 
-        this.nextSlider = function () {
-            if(curentSlider === [...getImages].length-1){
-                curentSlider = 0;
-            }else {
-                curentSlider++;
-            }
-            [...getImages].forEach(function (image) {
-                image.classList.remove("showing");
-            });
-            [...getImages][curentSlider].classList.add("showing");
+    this.nextSlider = function() {
+      if (curentSlider === [...getImages].length - 1) {
+        curentSlider = 0;
+      } else {
+        curentSlider++;
+      }
+      [...getImages].forEach(function(image) {
+        image.classList.remove("showing");
+      });
+      [...getImages][curentSlider].classList.add("showing");
+    };
 
-        };
+    this.prevSlider = function() {
+      if (curentSlider === 0) {
+        curentSlider = [...getImages].length - 1;
+      } else {
+        curentSlider--;
+      }
+      [...getImages].forEach(function(image) {
+        image.classList.remove("showing");
+      });
+      [...getImages][curentSlider].classList.add("showing");
+    };
+  }
 
-        this.prevSlider = function () {
-            if(curentSlider === 0){
-                curentSlider = [...getImages].length-1;
-            }else {
-                curentSlider--;
-            }
-            [...getImages].forEach(function (image) {
-                image.classList.remove("showing");
-            });
-            [...getImages][curentSlider].classList.add("showing");
-        }
-    }
+  const pageSlider = new Slider();
+  let slideInterval = setInterval(pageSlider.nextSlider, 5000);
 
-    const pageSlider = new Slider();
-    let slideInterval = setInterval(pageSlider.nextSlider, 5000);
-
-    document.querySelector(".slider-wrapper").addEventListener("mouseover", function () {
-        clearInterval(slideInterval);
+  document
+    .querySelector(".slider-wrapper")
+    .addEventListener("mouseover", function() {
+      clearInterval(slideInterval);
     });
-    document.querySelector(".slider-wrapper").addEventListener("mouseout", function () {
-        slideInterval = setInterval(pageSlider.nextSlider, 5000);
+  document
+    .querySelector(".slider-wrapper")
+    .addEventListener("mouseout", function() {
+      slideInterval = setInterval(pageSlider.nextSlider, 5000);
     });
-    document.querySelector(".prev").addEventListener("click", pageSlider.prevSlider);
-    document.querySelector(".next").addEventListener("click", pageSlider.nextSlider);
+  document
+    .querySelector(".prev")
+    .addEventListener("click", pageSlider.prevSlider);
+  document
+    .querySelector(".next")
+    .addEventListener("click", pageSlider.nextSlider);
 
-    document.querySelector(".icon-container").addEventListener("click", function () {
-        document.querySelector(".page-nav").classList.toggle("visible");
-    })
-
+  document
+    .querySelector(".icon-container")
+    .addEventListener("click", function() {
+      document.querySelector(".page-nav").classList.toggle("visible");
+    });
 });
